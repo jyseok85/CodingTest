e68323ee51d7755fea6e7f1ae9e501485434cb03
연석 조 <jyseok@duzonbizon.onmicrosoft.com>
2021-07-16 16:35:19 +0900

    가장큰수 완료

--- 정렬/가장큰수.cs
+++ 정렬/가장큰수.cs
@@ -10,43 +10,126 @@ namespace CodingTest
     {
         public 가장큰수()
         {
-            solution(new int[] { 10, 15, 62, 6, 67,22,2 });
+            //solution(new int[] { 10, 15, 62, 6, 67,22,2,695,673,69 });
+
+            //solution(new int[] { 10, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            //solution(new int[] { 200,20,2 });
+            solution(new int[] { 0,0,0,0 });
+            //987654321101000
         }
+
+
+
         public string solution(int[] numbers)
         {
-            Dictionary<int, List<int>> dic = new Dictionary<int, List<int>>();
-            //앞자리만 잘라서 가장 큰 수를 찾는다.
-
-            //가져온 가장 큰수중에 제일 작은수(단위가 작은것)부터 문자열로 붙인다.
-
-            //그다음 큰수를 가져온다. 
-
-            //작은단위부터 붙인다 계속 반복
-
-            foreach(int number in numbers)
+            List<NumberClass> list = new List<NumberClass>();
+          
+            foreach (int number in numbers)
             {
-                int key = int.Parse(number.ToString().Substring(0, 1));
-                if (dic.Keys.Contains(key) == false)
-                    dic.Add(key, new List<int>());
-                dic[key].Add(number);
-            }
-            var items = from pair in dic
-                        orderby pair.Key descending
-                        select pair;
-
-            foreach (KeyValuePair<int, List< int>> item in items)
-            {
-                Dictionary<int, int> lengthDic = new Dictionary<int, int>();
-                foreach(int num in item.Value)
+                //정렬을 하기 위하여 같은 문자를 더해서 6자리 일치 시켜준다. 
+                string modiStr = number.ToString();
+                while(modiStr.Length < 6)
                 {
-                    lengthDic.Add(num.ToString().Length, num);
+                    modiStr += number;
                 }
-                item.Value.Sort();
-
+                modiStr = modiStr.Substring(0, 6);
+                NumberClass num = new NumberClass();
+                num.key = int.Parse(modiStr);
+                num.original = number;
+                list.Add(num);
             }
 
+            //6자리로 만든 수로 정렬을 한다. 
+            var items = list.OrderByDescending(dict => dict.key);
             string answer = "";
+            
+            list = items.ToList();
+            //6자리로 만든 숫자중에 중복되는 값이 있는경우 원본 값으로 정렬을 해서 더한다.
+            for (int i = 0; i < list.Count; i++)
+            {
+                answer += 중복키계산(ref i, list);
+               // answer += list[i].original;
+            }
+
+            //0체크
+            List<char> checkList = new List<char>();
+            checkList.Add('1');
+            checkList.Add('2');
+            checkList.Add('3');
+            checkList.Add('4');
+            checkList.Add('5');
+            checkList.Add('6');
+            checkList.Add('7');
+            checkList.Add('8');
+            checkList.Add('9');
+
+            bool onlyZero = true;
+            foreach(char c in checkList)
+            {
+                if (answer.Contains(c))
+                {
+                    onlyZero = false;
+                    break;
+                }
+            }
+
+            if (onlyZero)
+                return "0";
             return answer;
+        }
+
+        private string 중복키계산(ref int index , List<NumberClass> list)
+        {
+            List<int> dlist = new List<int>();
+            int plusCount = 0;
+            for (int i = index; i < list.Count; i++)
+            {
+                int currentKey = list[i].key;
+                dlist.Add(list[i].original);
+                if (list.Count <= i + 1)
+                    break;
+
+                int nextKey = list[i + 1].key;
+
+                if (currentKey == nextKey)
+                {
+                    plusCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            index += plusCount;
+            if (dlist.Count > 1)
+            {
+                dlist.Sort();
+            }
+                        
+            string returnString = string.Join("", dlist);
+            return returnString;
+        }
+        public class NumberClass
+        {
+            public int key { get; set; }
+            public int original { get; set; }
+        }
+
+
+        //속도는 5배정도 느린대신 메모리 30%정도 적게 사용.
+        private string BestSolution(int[] number)
+        {
+            Array.Sort(number, (x, y) =>
+            {
+                string xy = x.ToString() + y.ToString();
+                string yx = y.ToString() + x.ToString();
+                return yx.CompareTo(xy);
+            });
+
+            if (number.Where(_ => _ == 0).Count() == number.Length)
+                return "0";
+            else
+                return string.Join("", number);
         }
     }
 }
